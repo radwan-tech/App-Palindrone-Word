ca750fc99da9b8f24c7dfb6bf13278d50cae6ebb
PC <PC@ZICO>
2025-03-22 22:08:29 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+
+namespace Palindrone_Word
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool  isPalindrone = true;
+            
+            while (true)
+            {
+                Console.Write("Please enter The Word To Check:");
+                string word = Console.ReadLine();
+               var aword= word.ToLower();
+                for (int i = 0; i < aword.Length / 2; i++)
+                {
+
+
+                    if (aword[i] != aword[word.Length - i - 1])
+                    {
+                        isPalindrone = false;
+                        break;
+                    }
+                }
+                Console.WriteLine($"is Palindrone : {isPalindrone}");
+
+            }
+        }
+    }
+}
